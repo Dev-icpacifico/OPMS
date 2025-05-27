d0ea78fca3f7b58281ccb326b6f2101df057b00f
LuisPizarro04 <luispizarroalvarez8@gmail.com>
2025-05-27 15:22:27 -0400

    Version: 1era Versión funcional del sistema SaraMS

--- OPMS/settings.py
+++ OPMS/settings.py
@@ -108,12 +108,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_TZ = True
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'es-cl'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
